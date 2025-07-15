ce614f014bd8a72f2c9dfdfd7534eede75bc3ada
223Win <112732681+223Win@users.noreply.github.com>
2025-07-14 22:08:29 -0500

    idk how this happened

--- HookFunctionTestingV2.lua
+++ HookFunctionTestingV2.lua
@@ -162,7 +162,7 @@ Test("[L]->[RC]",function()
     C.ToHookWithRC = game.GetService
 
     local old
-    old = hookfunction(C.ToHookL,C.ToHookWithL)
+    old = hookfunction(C.ToHookL,C.ToHookWithRC)
     
     if C.ToHookL("ReplicatedFirst") ~= game:GetService("ReplicatedFirst") then
         return 0, "Failed to hook - Did not return the specified Roblox Service?"
